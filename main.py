57ebf90772f7a4762f5fc5e61c1a996d19a3449b
Nate Ruppert <nathaniel.ruppert@sce.com>
2021-06-15 16:50:10 -0700

    added creation of simulation files (header only)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+
+
 def list_types():
     max_cells = [8, 9, 10]
+    a = []
     for i in max_cells:
         solar_cell = [x for x in range(1, i + 1) if (i % x == 0 and x <= i)]
         for cellA in solar_cell:
@@ -7,8 +11,20 @@ def list_types():
                 if cellA == cellB and cellA * cellB != i:
                     continue
                 elif cellA * cellB == i:
-                    print(f'[{i}] Solar Arrangement {cellA}x{cellB}')
+                    print(f"[{i}] Solar Arrangement {cellA}x{cellB}")
+                    a.append([cellA, cellB])
+    return a
 
 
-if __name__ == '__main__':
-    list_types()
+if __name__ == "__main__":
+
+    for col, row in list_types():
+        print(f"{col}x{row}.cir")
+        os.makedirs("Output", exist_ok=True)
+        with open(f"Output/{col}x{row}.cir", "w+") as f:
+            f.write("* Files designed by circuit-sim by Nate Ruppert\n")
+            f.write(f"* Circuit Simulation of {col}x{row} Solar Cell Arrangement\n")
+            # Required for project
+            temp = 30  # TODO future support for changing temps
+            f.write(".include cell_2.lib\n")
+            f.write(f".option temp={temp}")
