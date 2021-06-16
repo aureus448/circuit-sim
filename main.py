2279e8f265dcf8ed98da6734df1dd1e4b20dcf7c
Nate Ruppert <nathaniel.ruppert@sce.com>
2021-06-15 18:34:52 -0700

    added dataset choice filtering and added indicating of file contents at top of file in creation process

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pathlib
 def list_types():
     max_cells = [8, 9, 10]
     a = []
+    sets_to_make = ["1x10", "2x4", "2x5", "3x3", "4x2", "5x2", "10x1"]
     for i in max_cells:
         solar_cell = [x for x in range(1, i + 1) if (i % x == 0 and x <= i)]
         for cellA in solar_cell:
@@ -13,6 +14,8 @@ def list_types():
                 if cellA == cellB and cellA * cellB != i:
                     continue
                 elif cellA * cellB == i:
+                    if f"{cellA}x{cellB}" not in sets_to_make:
+                        continue
                     print(f"[{i}] Will Design Solar Arrangement {cellA}x{cellB}")
                     a.append([cellA, cellB])
     return a
@@ -26,6 +29,62 @@ def create_file(path, row, col, shade, full_val, shade_val, temp):
         f.write(
             f"* Circuit Simulation of {row} Series x {col} Parallel Solar Cell Arrangement\n"
         )
+        f.write(f"* {shade} Shade {row*col-shade} No-Shade File\n")
+        # Required for project
+        f.write(".include cell_2.lib\n")
+        f.write(f".option temp={temp}")
+        f.write("\n")  # <br>
+
+        # Adds data for each cell depending on how many cells to add
+        for i in range(col):
+            f.write(f"\n*** Start of Column {i+1:02d}\n\n")
+
+            for j in range(1, row + 1):
+                # Define Start
+
+                start = f"{i}{j}"
+
+                # Define End
+                end = f"{i}{j+1}"
+                # Cell A and Cell B design
+                # xcell_01_12 12 01 1201
+                # xcell_12_00 0  12 0012
+                # virrad_11_12  1201  12 dc 1000
+                # virrad_12_00  0012  0  dc 1000
+                # Formats similar to above - sorta
+                f.write(f"** Cell {j:02d} [Col {i+1:02d}]\n")
+                f.write(
+                    f'xcell_{start}_{end} {end if j != row else "0"} {start if j != 1 else "01"} '
+                    f"{end}{start} cell_2 params:area=49  j0=16E-20 j02=1.2E-12\n"
+                )
+                f.write("+ jsc=30.5E-3 rs=28e-3 rsh=100000\n")
+                # Voltage changed to use shade
+                if (i + 1) * j <= shade:  # Shade number indicates how many to shade
+                    f.write(
+                        f'virrad_{start}_{end}  {end}{start} {end if j != row else "0"} dc {shade_val}\n'
+                    )
+                else:
+                    f.write(
+                        f'virrad_{start}_{end}  {end}{start} {end if j != row else "0"} dc {full_val}\n'
+                    )
+                f.write("\n")  # <br>
+        f.write("\nvbias 01 0 dc 0\n")  # Add voltage probe
+        f.write(".plot dc i(vbias)\n")  # Plot current based on voltage probe
+        f.write(
+            f".dc vbias 0 {1.05*row} 0.01\n"
+        )  # Indicate voltage probe voltage characteristics
+        f.write(".probe\n.end\n")  # End file
+
+
+def create_special_file(
+    path, row, col, shade, full_val, shade_val, temp, type, type_num
+):
+    with open(f"{path}/{row}x{col}_{type_num}_{type}.cir", "w+") as f:
+        f.write("* Files designed by circuit-sim by Nate Ruppert\n")
+        f.write(
+            f"* Circuit Simulation of {row} Series x {col} Parallel Solar Cell Arrangement\n"
+        )
+        f.write(f"* {shade} Shade {row*col-shade} No-Shade File")
         # Required for project
         f.write(".include cell_2.lib\n")
         f.write(f".option temp={temp}")
@@ -79,6 +138,7 @@ if __name__ == "__main__":
     data_sets = [
         [key, config[key]["temps"]] for key in config.keys() if key != "DEFAULT"
     ]
+    file_sets = list_types()
 
     for set, temps in data_sets:
 
@@ -97,18 +157,18 @@ if __name__ == "__main__":
                 temp_sets += results
         print(f"Generating {set} for temperatures: {temp_sets}")
         for temp in temp_sets:
-            for row, col in list_types():
-                sets_to_make = ["1x10", "2x4", "2x5", "3x3", "4x2", "5x2", "10x1"]
-                if f"{row}x{col}" not in sets_to_make:
-                    continue
+            for row, col in file_sets:
                 high, low = map(int, set.split("-"))
-
+                filepath = f"Output/{high}-{low}/Temp{temp}/{row}x{col}"
+                os.makedirs(
+                    pathlib.PurePath(filepath),
+                    exist_ok=True,
+                )
+                with open("cell_2.lib", "r") as file_r, open(
+                    filepath + "/cell_2.lib", "w"
+                ) as f:
+                    f.write(file_r.read())  # write out the file to correct pathing
                 for n_shade in range(row * col + 1):
-                    filepath = f"Output/{high}-{low}/Temp{temp}/{row}x{col}"
-                    os.makedirs(
-                        pathlib.PurePath(filepath),
-                        exist_ok=True,
-                    )
                     create_file(
                         filepath,
                         row,
