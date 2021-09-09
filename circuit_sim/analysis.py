9c181472262633e4d2d988fa33434708a80bb867
Nate Ruppert <nate.ruppert@csu.fullerton.edu>
2021-09-08 17:39:47 -0700

    added per-cell data collection
    - analysis part of library reconfigured to work with per-cell data based on file naming scheme

--- circuit_sim/analysis.py
+++ circuit_sim/analysis.py
@@ -22,7 +22,8 @@ from a normal installation, otherwise this script will not work.
 import logging
 import os
 import pathlib
-import re
+
+# import re
 import subprocess
 import time
 from queue import Queue
@@ -131,7 +132,7 @@ def run_simulations(path: pathlib.PurePath, data_name: List[str]) -> None:
         # time.sleep(10)
         start = time.perf_counter()
         run_ltspice(commands)
-        logging.info(f"Done in {time.perf_counter()-start:.2f}s")
+        logging.info(f"Done in {time.perf_counter() - start:.2f}s")
 
 
 def data_analysis(path: pathlib.PurePath, data_name) -> None:
@@ -152,27 +153,34 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
             The format of directory paths expected is similar to what is created by function
             ``create_files``.
     """
-    i = 1  # tracker of how many circuit(s) are run (total to support full set combination)
+    cell_identifier = 1  # tracker of how many circuit(s) are run (total to support full set combination)
     dataframe_list = []
     dataframe_list_special = []
     name_list = []
     bad_data = False
     for dataset in os.scandir(path):
         if dataset.is_dir() and dataset.name in data_name:
+            # used for each file later
+            full, shade = map(int, dataset.name.split("-"))
+
             logging.info(f"Running Data Analysis on {dataset.name}")
             temp_list = []
             temp_list_special = []
             for directory in os.scandir(dataset.path):
                 if directory.is_dir():
+                    # used for each file later
+                    temperature = int(directory.name.split("Temp")[-1])
+
                     logging.debug(f"Main Directory: {dataset.name}/{directory.name}")
+                    start = time.perf_counter()
                     logging.info(
                         f"Generating data analysis for: {dataset.name} [{directory.name}]"
                     )
-                    gigantor = pd.DataFrame()  # normal
-                    specialdf = pd.DataFrame()  # special
+                    gigantor = []  # normal
+                    specialdf = []  # special
                     # check for special as that is last file created for each set
                     if os.path.exists(
-                        f"Output/Data/{dataset.name}/{dataset.name}-{directory.name}-Special.csv"
+                        f"Output/Data/{dataset.name}-{directory.name}-Special.csv"
                     ):
                         logging.debug(
                             f"Data for set {dataset.name}-{directory.name} already exists"
@@ -180,6 +188,10 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
                         continue  # skip already done sets
                     for sub_dir in os.scandir(directory.path):
                         if sub_dir.is_dir():
+                            # Variables used for each file
+                            series, parallel = map(int, sub_dir.name.split("x"))
+                            cell_num = series * parallel
+
                             logging.debug(
                                 f"Generating for: {dataset.name}/{directory.name}/{sub_dir.name} [{dataset.name}]"
                             )
@@ -187,6 +199,13 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
                                 sub_dir.path
                             ):  # rescan directory after modifying and creating .raw files
                                 if file.is_file() and file.name.endswith("raw"):
+                                    # start_file = time.perf_counter()
+                                    special = False
+                                    special_num = 0
+                                    if "Shading" not in file.name:
+                                        special = True
+                                        special_num = int(file.name.split("_")[1])
+
                                     logging.debug(
                                         f"Creating Datasheet Output from {file.name}"
                                     )
@@ -207,6 +226,10 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
                                         bad_data = True  # don't create final report and warn user
                                         continue
 
+                                    # logging.info(
+                                    #     f'File load took {(time.perf_counter() - start_file) * 1000:.2f} milliseconds'
+                                    # )
+                                    # start_file = time.perf_counter()
                                     vbias = lt_data.get_data("vbias")
 
                                     # Totals for full array combined (Thevenins)
@@ -218,71 +241,119 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
                                     # Individual current for each cell?
                                     # This current matches Ivbias [_ for _ in lt_data.getVariableNames() if ':300' in _]
 
-                                    out = pd.DataFrame()
-                                    out["Voltage (V)"] = vbias
-                                    out["Current (A)"] = list(
-                                        map("{:.2f}".format, I_Vbias)
-                                    )
-                                    out["Power (W)"] = list(
-                                        map("{:.2f}".format, P_Vbias)
-                                    )
-
-                                    filename = file.name.split(".")[0] + ".txt"
-                                    rx = re.compile(r"-?\d+(?:\.\d+)?")
-                                    rtemp = re.compile(r"\d+")
-                                    contentList = list(map(int, rx.findall(filename)))
-                                    cells = (
-                                        contentList[2] if len(contentList) > 2 else 0
-                                    )
-                                    temp = list(
-                                        map(int, rtemp.findall(directory.name))
-                                    )[0]
-                                    out["Full Voltage"] = dataset.name.split("-")[0]
-                                    out["Shade Voltage"] = dataset.name.split("-")[1]
-                                    out["Temperature"] = temp
-                                    out["File Name"] = filename
-                                    out["# Of Cells"] = contentList[0] * contentList[1]
-                                    out["Series Cells"] = contentList[0]
-                                    out["Parallel Cells"] = contentList[1]
-
-                                    special = False
-                                    if "Shading" not in filename:
-                                        special = True
-
-                                    out["Type"] = (
+                                    type = (
                                         "Open"
-                                        if special and "Open" in filename
+                                        if "Open" in file.name
                                         else "Short"
                                         if special
                                         else "Shaded"
                                     )
-                                    out["Shaded Cells"] = cells if not special else 0
-                                    out["Shading %"] = (
-                                        f"{((cells / (contentList[0] * contentList[1])) * 100):.2f}"
-                                        if not special
-                                        else 0
-                                    )
-                                    out["IsShade"] = (
-                                        1 if cells > 0 and not special else 0
-                                    )
+                                    frame = {
+                                        "Voltage (V)": vbias.round(2),
+                                        "Current (A)": I_Vbias.round(2),
+                                        "Power (W)": P_Vbias.round(2),
+                                        "Full Voltage": full,
+                                        "Shade Voltage": shade,
+                                        "Temperature": temperature,
+                                        "File Name": file.name.split(".")[0],
+                                        "# Of Cells": cell_num,
+                                        "Series Cells": series,
+                                        "Parallel Cells": parallel,
+                                        "Type": type,
+                                        "Solar Panel ID": cell_identifier,
+                                    }
 
-                                    out["Solar Panel ID"] = i
+                                    # Per-cell data collection
+                                    for i in range(parallel):
+                                        # print(f'Parallel: {i+1}')
+                                        # For each column we write to the file each cell
+                                        for j in range(1, series + 1):
+                                            # print(f'\tSeries: {j}')
+                                            cell_n = (
+                                                i * series + j
+                                            )  # current cell out of all cells
+                                            if (
+                                                type == "Open"
+                                                and (i + 1) > parallel - special_num
+                                            ):
+                                                frame[
+                                                    f"Cell {cell_n} Power"
+                                                ] = 0.00  # open cell = 0 power
+                                                continue
+                                            # short has no issues - only open
+                                            # elif type =='Short' and cell_n > (parallel*series-special_num):
+                                            #     frame[f'Cell {cell_n} Power'] = 0.00 # short cell = 0 power
+                                            #     continue
 
+                                            # collection of current data logic
+                                            cell_a = f"{i}{j}"
+                                            cell_b = f"{i}{j + 1}"
+
+                                            # collection of voltage data logic
+                                            volt_a = f"{cell_a if j != 1 else '01'}"
+                                            volt_b = (
+                                                f"{i}{j + 1}" if cell_n < series else ""
+                                            )
+
+                                            # format for grab from ltspice library
+                                            prim = f"V({volt_a})"  # guarantees proper zfill
+                                            sec = (
+                                                f"V({volt_b})"
+                                                if cell_n < series
+                                                else ""
+                                            )
+                                            cell = f"Ix(cell_{cell_a}_{cell_b}:300)"  # 300 is correct port for diode
+                                            # print(prim, sec)
+                                            if sec:
+                                                # print(f'\t\tCell {j}: = {prim}-{sec}'
+                                                #       f'*{cell}')
+                                                v_in = lt_data.get_data(f"{prim}")
+                                                v_out = lt_data.get_data(f"{sec}")
+                                                cell_data = lt_data.get_data(f"{cell}")
+                                                result = (v_in - v_out) * cell_data
+                                                # print('done')
+                                            else:
+                                                # print(
+                                                #     f'\t\tCell {j}: = {prim}'
+                                                #     f'*{cell}'
+                                                # )
+                                                v_in = lt_data.get_data(f"{prim}")
+                                                cell_data = lt_data.get_data(f"{cell}")
+                                                result = (
+                                                    v_in * cell_data
+                                                )  # vout is ground - not needed in equation
+
+                                            # frame[f'Cell {cell_n} Current'] = cell_data.round(2) - equivalent to Ith
+                                            frame[
+                                                f"Cell {cell_n} Power"
+                                            ] = result.round(2)
+
+                                    out = pd.DataFrame(data=frame)
+
+                                    # logging.info(
+                                    #     f'File derivation took '
+                                    #     f'{(time.perf_counter() - start_file) * 1000:.2f} milliseconds'
+                                    # )
+                                    # start_file = time.perf_counter()
                                     if special:
-                                        specialdf = pd.concat(
-                                            [specialdf, out], ignore_index=True
-                                        )
+                                        specialdf.append(out)
                                     else:
-                                        gigantor = pd.concat(
-                                            [gigantor, out], ignore_index=True
-                                        )
-                                    i += 1  # increment number of solar panels run on
+                                        gigantor.append(out)
+                                    # logging.info(f'File took '
+                                    #              f'{(time.perf_counter()-start_file)*1000:.2f} milliseconds')
+                                    # increment number of solar panels run on
+                                    cell_identifier += 1
                     os.makedirs(f"Output/Data/{dataset.name}/", exist_ok=True)
 
                     logging.debug(f"Creating file {dataset.name}-{directory.name}.csv")
+
+                    # defined as lists
+                    gigantor = pd.concat(gigantor, ignore_index=True)
+                    specialdf = pd.concat(specialdf, ignore_index=True)
                     pd.concat([gigantor, specialdf], ignore_index=True).to_csv(
                         f"Output/Data/{dataset.name}/{dataset.name}-{directory.name}.csv"
                     )
+                    logging.debug(f"Took {time.perf_counter() - start:.2f} seconds")
                     # gigantor.to_csv(
                     #     f"Output/Data/{dataset.name}/{dataset.name}-{directory.name}-Shaded.csv",
                     #     index=False,
@@ -295,22 +366,32 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
                     temp_list_special.append(specialdf)
 
             if len(temp_list) > 0:
-                logging.info("Creating set dataframe")
-                mega_set = pd.concat(temp_list, ignore_index=True, copy=False)
+                logging.info(f"Creating set dataframe for: {dataset.name}")
+                start_create = time.perf_counter()
+                mega_set = pd.concat(temp_list, ignore_index=True, copy=True)
                 mega_set.to_csv(f"Output/Data/{dataset.name}-Shading.csv")
                 mega_set_special = pd.concat(
-                    temp_list_special, ignore_index=True, copy=False
+                    temp_list_special, ignore_index=True, copy=True
                 )
                 mega_set_special.to_csv(f"Output/Data/{dataset.name}-Special.csv")
                 dataframe_list.append(mega_set)
                 dataframe_list_special.append(mega_set_special)
                 name_list.append(f"{dataset.name}")
-                # Create subset xlsx
-                with pd.ExcelWriter(f"Output/Data/{dataset.name}.xlsx") as writer:
-                    mega_set.to_excel(writer, sheet_name=f"{dataset.name}-Shading")
-                    mega_set_special.to_excel(
-                        writer, sheet_name=f"{dataset.name}-Special"
-                    )
+                # Create subset xlsx - takes way too long I gave up
+                # wb = Workbook()
+                # data_norm = [mega_set.columns.to_list()] + mega_set.values.tolist()
+                # data_spec = [mega_set_special.columns.to_list()] + mega_set_special.values.tolist()
+                # ws = wb.new_sheet(f"{dataset.name}-Shading", data=data_norm)
+                # ws = wb.new_sheet(f"{dataset.name}-Special", data=data_spec)
+                # wb.save(f"Output/Data/{dataset.name}.xlsx")
+                # with pd.ExcelWriter(f"Output/Data/{dataset.name}.xlsx") as writer:
+                #     mega_set.to_excel(writer, sheet_name=f"{dataset.name}-Shading")
+                #     mega_set_special.to_excel(
+                #         writer, sheet_name=f"{dataset.name}-Special"
+                #     )
+                logging.info(
+                    f"Dataset output took {time.perf_counter() - start_create:.2f} seconds"
+                )
             elif os.path.exists(f"Output/Data/{dataset.name}-Special.csv"):
                 mega_set = pd.read_csv(f"Output/Data/{dataset.name}-Shading.csv")
                 mega_set_special = pd.read_csv(
@@ -329,19 +410,19 @@ def data_analysis(path: pathlib.PurePath, data_name) -> None:
             "Please re-run the tool to re-simulate the error data."
         )
         return
-    logging.info("Creating complete dataframe and .xlsx file - this will take a bit")
+    logging.info("Creating complete dataframe - this will take a bit")
     # Concatenate everything
     mega_set = pd.concat(dataframe_list, ignore_index=True, copy=False)
     mega_set.to_csv("Output/all_cell_data-Shading.csv")
     mega_set_special = pd.concat(dataframe_list_special, ignore_index=True, copy=False)
-    mega_set_special.to_csv("Output/all_cell_data-Shading.csv")
+    mega_set_special.to_csv("Output/all_cell_data-Special.csv")
     # print(name_list)
     # print(dataframe_list)
-    with pd.ExcelWriter("Output/all_cell_data.xlsx") as writer:
-        for i in range(len(dataframe_list)):
-            print("Running on", name_list[i])
-            dataframe_list[i].to_excel(writer, sheet_name=f"{name_list[i]}-Shading")
-            dataframe_list_special[i].to_excel(
-                writer, sheet_name=f"{name_list[i]}-Special"
-            )
+    # with pd.ExcelWriter("Output/all_cell_data.xlsx") as writer: - takes wayyyyyy to long I give up
+    #     for i in range(len(dataframe_list)):
+    #         print("Running on", name_list[i])
+    #         dataframe_list[i].to_excel(writer, sheet_name=f"{name_list[i]}-Shading")
+    #         dataframe_list_special[i].to_excel(
+    #             writer, sheet_name=f"{name_list[i]}-Special"
+    #         )
     logging.info("Dataframe finished - Analysis complete")
